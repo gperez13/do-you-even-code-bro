b75851af68079a989cac9a54a15c7a9040668bd2
Giovanni Perez <giovanniperez@Giovannis-MacBook-Pro.local>
2017-09-18 10:06:12 -0500

    we started the game coding

---
+++ doYouEvenCodeBro.js
@@ -0,0 +1,31 @@
+let player = {
+	name: 'Broslaya',
+	health: 100,
+	weapon: books',
+	costume: ['glasses', 'pants', 'shirt', 'pocket protector', 
+	'socks', 'shoes']
+	lives: 3,
+	location:
+}
+
+let opponent = {
+	type: {level1: 'wall street bro', level2: 'frat bro', level3: 'brogrammer in denial', bossLevel: 'Johnny Bravo'}
+	health: 50,
+	location:  
+}
+
+const game = () =>{
+	if (numOfOpponents === 0){
+		return newLevel()
+	}
+} else if( numOfLives === 0){
+	alert('game over')
+	return stopGame()
+}
+
+
+
+
+
+
+
